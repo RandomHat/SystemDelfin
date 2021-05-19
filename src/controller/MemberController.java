5e9c650872ded81e24d5411f4a49ef72dee7312b
Roswall <simonroswall@gmail.com>
2021-05-19 22:09:13 +0200

    added functionality to MemberList.java

--- src/controller/MemberController.java
+++ src/controller/MemberController.java
@@ -1,29 +1,5 @@
 package controller;
 
-import model.Member;
-
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class MemberController {
-    Member member1 = new Member("Dennis", "19 juli 1998", "mand", true, 69696969,
-            "dennis@gmail.com", "dennisvej69", 1);
-}
-
-public static void AddMember() throws FileNotFoundException{
-    Scanner newmember = new Scanner(System.in);
-    System.out.println("Enter name: ");
-    String name = newmember.nextLine();
-    System.out.println("Enter Birthday: ");
-    String birthday = newmember.nextLine();
-}
-
-public static void EditMember() throws FileNotFoundException{
-
-
-}
-
-public static void DeleteMember() throws FileNotFoundException{
-
 
 }
