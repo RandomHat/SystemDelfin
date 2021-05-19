09d5b2aecb2865283563b3bebebfba1d9e9af4e8
Mark Hansen <Mark024e@stud.kea.dk>
2021-05-19 22:31:54 +0200

    Comparable<Result> tilføjet til Result

--- src/model/TournamentResult.java
+++ src/model/TournamentResult.java
@@ -2,7 +2,7 @@ package model;
 
 import java.util.Date;
 
-public class TournamentResult extends Result implements Comparable<TournamentResult>{
+public class TournamentResult extends Result{
 
     private int placement;
     private String tournament;
@@ -26,7 +26,7 @@ public class TournamentResult extends Result implements Comparable<TournamentRes
     }
 
     @Override
-    public int compareTo(TournamentResult o) {
+    public int compareTo(Result o) {
 
             if (super.getTime() > o.getTime()) {
                 return 1;
