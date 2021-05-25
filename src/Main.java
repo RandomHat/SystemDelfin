82ee8514b3b1c7c10c9e0e4d0343a6f85a51b00a
Mark Hansen <Mark024e@stud.kea.dk>
2021-05-25 10:28:40 +0200

    x

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 import View.ViewController;
+import model.SwimDiscipline;
+import model.TournamentResult;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
+
         ViewController run = new ViewController();
         run.mainMenu();
+
     }
 }
